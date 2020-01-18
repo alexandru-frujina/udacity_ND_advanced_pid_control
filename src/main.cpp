d5087bbff158b08f67c8fe022d188771190ee306
Alexandru <alexandru.frujina@gmail.com>
2020-01-18 20:51:04 +0200

    PID control with manual parameter adjustment

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include "json.hpp"
 #include "PID.h"
+#include <cmath>
 
 // for convenience
 using nlohmann::json;
@@ -37,8 +38,13 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
+  double tau_p = 0.16;
+  double tau_d = 1.9;
+  double tau_i = 0.0;
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
+  pid.Init(tau_p, tau_i, tau_d);
+
+  h.onMessage([&pid, &tau_p, &tau_d, &tau_i](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -64,6 +70,10 @@ int main() {
            *   Maybe use another PID controller to control the speed!
            */
           
+          pid.UpdateError(cte);
+
+          steer_value = pid.TotalError();
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
                     << std::endl;
